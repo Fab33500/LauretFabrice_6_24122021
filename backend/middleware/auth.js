9b45f3759b31ce8c656d446425940673a5042b70
Fab33500 <lauretfabrice2@protonmail.com>
2022-01-15 15:55:40 +0100

    modification de nom de variale

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(" ")[1];
-		const decodedToken = jwt.verify(token, process.env.BCRYPT_PASSWORD);
+		const decodedToken = jwt.verify(token, process.env.JWT_PASSWORD);
 		const userId = decodedToken.userId;
 		req.auth = { userId };
 
